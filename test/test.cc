0611e9a3f7e940aba3a1c8a75f00687f725cafba
Aidan Bailey <aidan.j.bailey@outlook.com>
2022-05-30 11:01:56 +0200

    Neaten up

--- test/test.cc
+++ test/test.cc
@@ -1,56 +1,40 @@
+#include "cppiperconfig.hh"
 #include "receiver.hh"
 #include "sender.hh"
-#include "cppiperconfig.hh"
 #include <chrono>
 #include <iostream>
+#include <ratio>
+#include <spdlog/common.h>
+#include <string>
 #include <thread>
 #include <tuple>
-#include <string>
-
 
 int main(int argc, char *argv[]) {
-  std::cout << CPPIPER_VERSION_MAJOR << std::endl;
-  cppiper::Sender server_sender("server");
-  char* msg = new char[1]{'A'};
-  for (int i = 0; i < 1000; i++)
-    bool success = server_sender.send(msg, 1);
-  delete [] msg;
+  std::cout << "cppiper v" << CPPIPER_VERSION_MAJOR << '.'
+            << CPPIPER_VERSION_MINOR << std::endl;
+  std::cout << "Dev Mode: " << DEV << std::endl;
+  if (DEV)
+    spdlog::set_level(spdlog::level::debug);
+  else
+    spdlog::set_level(spdlog::level::err);
+  cppiper::Sender server_sender("Server", "server");
+  cppiper::Receiver server_receiver("Client", "server");
+  std::vector<char> msg(5, 'A');
+  int testset_size = 20;
+  for (char i : msg)
+    std::cout << i;
+  std::cout << std::endl;
+  std::chrono::high_resolution_clock::time_point start =
+      std::chrono::high_resolution_clock::now();
+  for (int i = 0; i < testset_size; i++) {
+    server_sender.send(msg);
+    server_receiver.receive(true);
+  }
+  std::chrono::high_resolution_clock::time_point end =
+      std::chrono::high_resolution_clock::now();
+  std::chrono::duration<double, std::micro> delta = (end - start);
+  std::cout << "Avg of " << delta.count() / testset_size << "us/msg"
+            << std::endl;
   server_sender.terminate();
   return 0;
 }
-
-/*
-int main(int argc, char *argv[]) {
-  std::cout << argv[0] << " Version " << cppiper_VERSION_MAJOR << "."
-            << cppiper_VERSION_MINOR << std::endl;
-  std::cout << "Starting thread" << std::endl;
-  cppiper::Sender server_sender("server");
-  cppiper::Sender client_sender("client");
-  sleep(5);
-  std::cout << "Starting receiver" << std::endl;
-  cppiper::Receiver server_receiver("client");
-  cppiper::Receiver client_receiver("server");
-  // char *in{new char[2]{'A', 'B'}};
-  char in[3]{'A', 'B', 'C'};
-  std::chrono::high_resolution_clock::time_point start =
-      std::chrono::high_resolution_clock::now(); // get the time...
-  for (int i = 0; i < 100000; i++) {
-    server_sender.send(in, 3);
-    auto res = client_receiver.receive();
-    client_sender.send(std::get<0>(res), std::get<1>(res));
-    res = server_receiver.receive();
-  }
-  std::chrono::high_resolution_clock::time_point end =
-      std::chrono::high_resolution_clock::now(); //...get the time again
-  std::chrono::duration<double, std::milli> timeRequired = (end - start);
-  std::cout << "Finished in " << timeRequired.count() << "milliseconds"
-            << std::endl;
-  server_sender.terminate();
-  client_sender.terminate();
-  server_receiver.terminate();
-  client_receiver.terminate();
-  // delete [] in;
-  //
-  std::cout << "Finished..." << std::endl;
-}
-*/
