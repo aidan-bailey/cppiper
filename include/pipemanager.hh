a190c7b596f59e8a77d05db632118bbc4baf2da2
Aidan Bailey <aidan.j.bailey@outlook.com>
2022-06-03 13:10:42 +0200

    Add docs

--- include/pipemanager.hh
+++ include/pipemanager.hh
@@ -7,18 +7,48 @@
 
 namespace cppiper {
 
+//! Generate a random hex string of a specified length.
+/*!
+  \param len a specified length.
+  \return The hex string.
+ */
 std::string random_hex(int len);
 
+//! A pipe managing class.
+/*!
+  Manages the creation and removal of named pipes in a specific directory (pipe
+  directory).
+ */
 class PipeManager {
 private:
-  std::mutex lock;
-  const std::string pipedir;
+  const std::string pipedir; //!< Pipe directory path.
+  std::mutex lock;           //!< Pipe directory lock.
 
 public:
+  //! Deleted.
   PipeManager(void) = delete;
+
+  //! Construct a pipe manager.
+  /*!
+    \param pipedir a directory path for the pipes (this directory will be created
+    if it does not exist).
+   */
   PipeManager(std::string pipedir);
+
+  //! Make a new fifo pipe with a random name.
+  /*!
+    \return The path to the pipe.
+  */
   std::string make_pipe(void);
+
+  //! Remove a pipe from the pipe directory.
+  /*!
+    \param pipepath path to the pipe.
+    \return whether or not the removal was successful.
+  */
   bool remove_pipe(std::string pipepath);
+
+  //! Clear the pipe directory of pipes.
   void clear(void);
 };
 
